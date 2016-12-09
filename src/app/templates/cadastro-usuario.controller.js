47d61ec42f0589a820a630dac19e515e487b695e
glauroqj <glauro.juliani@hotmail.com>
2016-12-09 03:21:35 -0200

    cadastrando users

--- src/app/templates/cadastro-usuario.controller.js
+++ src/app/templates/cadastro-usuario.controller.js
@@ -10,13 +10,19 @@
 		vm.lista_cadastro_user = [];
 		$rootScope.boxTitle = 'cadastro de usuários';
 
-		vm.submit = function submit (user, password, acess, form) {
-			vm.lista_cadastro_user.push({name: user, pass: password, acess: acess});
-			console.log(vm.lista_cadastro_user)
-			Notification({message: 'Usuário '+user+' Cadastrado Com Sucesso'}, 'success');
+		vm.submit = function submit (user, password, acess) {
+			vm.usuarios_cadastrados = localStorage.getItem('cadastro-usuario');
 			
-		};
+			vm.lista_cadastro_user.push({name: user, pass: password, acess: acess});
+			vm.lista_cadastro_user_string = JSON.stringify(vm.lista_cadastro_user);
+			vm.lista_cadastro_user = vm.usuarios_cadastrados.concat(vm.lista_cadastro_user)
 
+			localStorage.setItem('cadastro-usuario', vm.lista_cadastro_user_string);
+
+			console.log(vm.lista_cadastro_user, vm.lista_cadastro_user_string)
+			Notification({message: 'Usuário '+user+' Cadastrado Com Sucesso'}, 'success');
+		};
+		console.log(vm.usuarios_cadastrados)
 
 	};
 
